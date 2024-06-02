beb7ae8a0303591bb73960bab4b90c2e305825fb
Yurii <yurii.halapup@gmail.com>
2024-06-03 02:19:01 +0300

    Fix migrations and add unique to email user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_24_123556) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_24_124001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_24_123556) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "comments", "posts"
