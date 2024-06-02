21f48ebc697c1571f460551d220615e862c3e9d8
Yurii <yurii.halapup@gmail.com>
2024-06-02 23:41:43 +0300

    Fix restriction from gem faker

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
+  PROHIBITED_KEYWORDS = Array.new(20) { Faker::Lorem.unique.word }
+
   belongs_to :post
   belongs_to :user
 
@@ -7,8 +9,8 @@ class Comment < ApplicationRecord
   private
 
   def content_does_not_contain_prohibited_keywords
-    prohibited_keywords = Array.new(20) { Faker::Lorem.unique.word }
-    prohibited_keywords.each do |keyword|
+    # prohibited_keywords = Array.new(20) { Faker::Lorem.unique.word }
+    PROHIBITED_KEYWORDS.each do |keyword|
       if content.downcase.include?(keyword)
         errors.add(:content, "contains prohibited keyword: #{keyword}")
         break
